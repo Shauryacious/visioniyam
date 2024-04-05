86aa9442c021a60a128dd06111d7b3ec134f7651
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-04-05 22:10:44 +0530

    Added GUI class
    Started Working on GUI 
    1. Start and Stop Button Added

--- python/main.py
+++ python/main.py
@@ -1,1 +1,32 @@
-print("Radhe Radhe")
+import cv2
+import mediapipe as mp
+import pyautogui
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class mouseController:
+    def start(self):
+        self.running = True
+
+    def stop(self):
+        self.running = False
+
+
+    def run(self):
+        self.root =tk.Tk()
+        self.root.title("Visioniyam")
+
+        self.startBtn =tk.Button(self.root,text="Start Capturing", command=self.start)
+        self.startBtn.pack(pady = 10)
+
+        self.stopBtn = tk.Button(self.root, text = "Stop Capturing", command=self.stop)
+        self.stopBtn.pack(pady= 5)
+
+        self.videoPanel = tk.Label(self.root)
+        self.videoPanel.pack()
+
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    visioniyam = mouseController()
+    visioniyam.run()
