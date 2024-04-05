22e44dd9f9df09fb928fa4736ede65979a71802d
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-04-06 01:14:26 +0530

    created NevBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import Home from "./component/Home.js";
+import NevBar from "./component/NevarBar.js";
 function App() {
   return (
+    <>
+    <NevBar></NevBar>
     <Home color={"black"}></Home>
+    </>
   );
 }
 
