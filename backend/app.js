be6311c920de0079506a34ad3a5b8a4be7d19191
Shauryacious <shauryaagrawal2718@gmail.com>
2024-04-05 21:16:10 +0530

    first commit - created server, added express,dotenv,path

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+const path = require('path');
+
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App is running on port ${port} in '${process.env.NODE_ENV}' mode`);
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Radhe Radhe!");
+});
