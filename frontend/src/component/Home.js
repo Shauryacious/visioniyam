a1b9c9143238cdfc41758fa38853bdbf3e56e54f
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-04-07 05:23:17 +0530

    increse the size

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -4,7 +4,7 @@ import ButtonWithBackground from './button';
 export default function Home(props) {
   return (
     <>
-      <div style={{ paddingTop: "120px", display: "flex", paddingBottom: '10px',gap:"300px" ,paddingLeft:"100px",height:"700px",marginBottom:"-200px"}}>
+      <div style={{ paddingTop: "120px", display: "flex", paddingBottom: '10px',gap:"300px" ,paddingLeft:"100px",height:"700px",marginBottom:"-100px"}}>
         <div style={{ position: "relative", top: "20px" }}>
           <div style={{ position: "relative", left: "100px", fontFamily: "poppins", fontWeight: "700", fontSize: '50px' }}>Control With Eye</div>
           <div style={{ width: '500px', position: "relative", left: "100px", fontFamily: "poppins", fontWeight: "400", fontSize: "16px" }}>Visionary is a free program that enables you to control the mouse pointer on your computer screen just by moving your head.</div>
